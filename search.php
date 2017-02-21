19740bee91cda157e1fe6081fdb9ccfa2343b310
Kevin Kwon <Kevin Kwon>
2017-02-20 19:48:31 -0800

    Started working on search bar

--- search.php
+++ search.php
@@ -15,7 +15,26 @@
 </head>
 <body>
   <?php echo $page_title;?>
-  THIS IS THE SEARCH PAGE
+  
+  	<section class="searchbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-sm-offset-3">
+					<div class="input-group stylish-input-group">
+						<input type="text" class="form-control"  placeholder="Search" >
+						<span class="input-group-addon">
+							<button type="submit">
+								search
+								<!-- <span class="glyphicon glyphicon-search"></span>  glyphicon isn't working-->
+							</button>  
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+		
+  
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.js"></script>
   <script>
