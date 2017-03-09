9ae0b9fa6d20ccd996e53616763f90438f4826fb
Iris Liu <irisylliu@gmail.com>
2017-03-08 21:31:37 -0800

    Favourites.php

--- favourites.php
+++ favourites.php
@@ -5,33 +5,33 @@ or die('Error connecting to MySql server.');
 ?>
 
 <html>
-	<head> 
-		<meta charset="utf-8">
-		<title>Favourites</title>
+  <head> 
+    <meta charset="utf-8">
+    <title>Favourites</title>
 
-		</head>
-		<body>
+    </head>
+    <body>
 
-		<h1> My Favourites</h1>
-		<?php
-		$query = "SELECT * FROM listoffavourites";
-		mysqli_query($db, $query) or die('Error querying database.');
-		
+    <h1> My Favourites</h1>
+    <?php
+    $query = "SELECT * FROM listoffavourites";
+    mysqli_query($db, $query) or die('Error querying database.');
+    
 
-		$result = mysqli_query($db, $query);
-		$row = mysqli_fetch_array($result);
+    $result = mysqli_query($db, $query);
+    $row = mysqli_fetch_array($result);
 
-		while($row = mysqli_fetch_array($result)){
-			echo $row['listname'] . '<br />';
+    while($row = mysqli_fetch_array($result)){
+      echo $row['listname'] . '<br />';
 
-		}
+    }
 
-		mysqli_close($db);
+    mysqli_close($db);
 
-		?>
+    ?>
 
 
-		</body>
+    </body>
 
-		</html>
+    </html>
 
