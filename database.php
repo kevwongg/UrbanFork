fe5b170476c01ba34c0edf63e23fc5d46a49c98a
Kevin Kwon <Kevin Kwon>
2017-03-10 15:50:55 -0800

    Added filter functionality for search

--- database.php
+++ database.php
@@ -1,7 +1,9 @@
 <?php
 // Connect to MySQL
-$con = mysqli_connect("localhost", "root", "", "urbanfork");
+$con = mysqli_connect("localhost", "root", "hello", "urbanfork");
+
 //Test Connection
 if(mysqli_connect_errno()){
 	echo 'Failed to connect to MySQL: '.mysqli_connect_error();
 }
+?>
