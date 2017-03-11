e53eb0cb86dcc9adbd014e03cbff33043905f691
Robert Hsieh <robertcdcj@gmail.com>
2017-03-10 23:49:59 -0800

    Update restaurant page UI

--- restaurant.php
+++ restaurant.php
@@ -19,6 +19,7 @@
 	
 	<?php echo $page_title;?>
 
+
 	<?php
 		$rname = $_GET['rname'];
 		// echo $rname;
@@ -35,21 +36,94 @@
 			$description = $row['description'];
 			$phone = $row['phone'];
 			
-			echo "<h1 class=\"jumbotron-heading\">$rname</h1>";
-			echo "<br>";
-			echo $location;
-			echo "<br>";
+			// echo "<h1 class=\"jumbotron-heading\">$rname</h1>";
+			// echo "<br>";
+			// echo $location;
+			// echo "<br>";
 			
-			echo "<br>";
-			echo $cuisine;
-			echo "<br>";
-			echo $description;
-			echo "<br>";
-			echo $phone;
+			// echo "<br>";
+			// echo $cuisine;
+			// echo "<br>";
+			// echo $description;
+			// echo "<br>";
+			// echo $phone;
 		}
 
 	?>
 
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>
+					<?php echo $rname ?>
+				</h1>
+				</br>
+				<dl>
+					<dt>
+						Cuisine
+					</dt>
+					<dd>
+						<?php echo $cuisine ?>
+					</dd>
+					<dt>
+						Description
+					</dt>
+					<dd>
+						<?php echo $description ?>
+					</dd>
+				</dl> 
+								
+				<div class="carousel slide" id="carousel-207906">
+					<ol class="carousel-indicators">
+						<li data-slide-to="0" data-target="#carousel-207906" class="active">
+						</li>
+						<li data-slide-to="1" data-target="#carousel-207906">
+						</li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="item active">
+							<img class="img-responsive center-block" alt="Carousel Bootstrap First" src="img/chinese.jpg" />
+							<div class="carousel-caption">
+								<h4>
+									First Thumbnail label
+								</h4>
+								<p>
+									Some text 1
+								</p>
+							</div>
+						</div>
+						<div class="item">
+							<img class="img-responsive center-block" alt="Carousel Bootstrap Second" src="img/french.jpg" />
+							<div class="carousel-caption">
+								<h4>
+									Second Thumbnail label
+								</h4>
+								<p>
+									Some text 2
+								</p>
+							</div>
+						</div>
+					</div>
+					<a class="left carousel-control" href="#carousel-207906" data-slide="prev">
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					</a>
+					<a class="right carousel-control" href="#carousel-207906" data-slide="next">
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					</a>
+				</div>
+
+				<br/>
+				<address>
+					 <strong><?php echo $rname ?></strong>
+					 <br/>
+					 <?php echo $location ?>
+					 <br />
+					 <abbr title="Phone">Phone:</abbr> <?php echo $phone ?>
+				</address>
+
+			</div>
+		</div>
+	</div>
 	
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.js"></script>  
