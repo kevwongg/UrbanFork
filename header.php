3c2c91f74b81199b7cb67c90c065072c91b1c194
Kevin Wong <kevinwong_895@hotmail.com>
2017-03-15 12:51:21 -0700

    Adding logout functionality

--- header.php
+++ header.php
@@ -18,7 +18,7 @@ $page_title = <<<EOD
       <li id="login" ><a href="login.php">Login/Register</a></li>
       <li id="search"><a href="search.php">Search Restaurants</a></li>
       <li id="Favourites"><a href="Favourites.php">Favourites</a></li>
-      <li><a href="#">Logout</a></li>
+      <li><a href="logout.php">Logout</a></li>
     </ul>
 
     </div>
