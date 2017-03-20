0872a6d432a8d13e1ee5e51b388d0d979ee17024
Kevin Wong <kevinwong_895@hotmail.com>
2017-03-20 10:46:28 -0700

    Admin login pages

---
+++ admin_login_process.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'database.php';
+
+// Check if form submitted
+if(isset($_POST['submit'])){
+	$email = mysqli_real_escape_string($con, $_POST['email']);
+	$password = mysqli_real_escape_string($con, $_POST['psw']);
+	// Validate input
+	if(!isset($email) || $email == '' 
+		|| !isset($password) || $password == ''){
+		$_SESSION['admin_errors'] = array("Fill in all fields!");
+		header("Location:login.php");
+		exit();
+	} else {
+		$result = mysqli_query($con, "SELECT * FROM admin WHERE email = '$email' AND password = '$password'") or die($mysqli->error());
+		if($result ->num_rows == 0){
+			$_SESSION['admin_errors'] = array("Incorrect user or password");
+			header("Location:adminlogin.php");
+			exit();
+		}
+        $username = mysqli_query($con, "SELECT id FROM admin WHERE email = '$email' AND password = '$password'") /*or die($mysqli->error())*/;
+        $row = mysqli_fetch_assoc($username); 
+        $_SESSION['admin_userid'] = $row['id'];
+        header("Location: index.php");
+	}
+}
