8f0c82f45d6ba708139e933ce27402fa50c03b8a
kevinw123 <kevinwong_895@hotmail.com>
2017-03-22 00:33:00 -0700

    Styling restaurant.php with css

--- restaurant.php
+++ restaurant.php
@@ -47,27 +47,30 @@
     </div>
 	<div class="text-center" id="menu"><h2>Menu</h2></div>
 	<?php 
-		$sql="SELECT * FROM menu WHERE rname = '" . $rname . "' AND location = '" . $location . "'ORDER BY type ASC"; 
+		$sql="SELECT * FROM menu WHERE rname = '" . $rname . "' AND location = '" . $location . "'ORDER BY type DESC"; 
 		$result = mysqli_query($con, $sql) or die(mysqli_error($con));
 		while($row = mysqli_fetch_array($result)){
 			$type = $row['type'];
 			echo "<div class = 'menu-type text-center'>";
 			echo $type;
 			echo "</div>";
-
+			echo "<br>";
 			$menu_query="SELECT * FROM contains JOIN dishes on dishes.dishid = contains.dishid WHERE rname = '" . $rname . "' AND location = '" . $location . "' AND type = '" . $type ."'"; 
 			$menu_result = mysqli_query($con, $menu_query) or die(mysqli_error($con));
 			echo "<div class = 'row'>";
 			while($menu_row = mysqli_fetch_array($menu_result)){
-				echo "<div class = 'col-sm-6 text-center'>";
-				echo "<div class = 'text-center menu-item'>";
+				echo "<div class = 'col-sm-6 text-center menu-item'>";
+				echo "<div class = 'text-center item-title'>";
 				echo $menu_row['dname'] . '... ';
 				echo $menu_row['price'];
 				echo "</div>";
+				echo "<div class = 'text-center item-description'>";
 				echo $menu_row['description'];
+				echo "</div>";
 				echo "</div>";
 			}
 			echo "</div>";
+			echo "<br>";
 		}
 	?>
   <script src="js/jquery.min.js"></script>
