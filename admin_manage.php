df6a381555a40a0e2584bdfa58db1b102ba17c1e
kevinw123 <kevinwong_895@hotmail.com>
2017-03-26 16:39:30 -0700

    Styling admin_manage page

--- admin_manage.php
+++ admin_manage.php
@@ -1,4 +1,28 @@
-<?php
-echo "Hello World";
+<!DOCTYPE html>
+<?php 
+// session_start();
+include("header.php");?>
 
-?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="">
+  <title>UrbanFork</title>
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/login.css" rel="stylesheet">
+</head>
+<body>
+  
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+  <script>
+    jQuery("#login").addClass("active");
+  </script>
+</body>
+</html>
