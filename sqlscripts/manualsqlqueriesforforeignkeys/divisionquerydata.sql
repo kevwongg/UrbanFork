5f4a1816928bbd8f2f30e120941bab4820e546ce
Kevin Wong <kevinwong_895@hotmail.com>
2017-03-26 19:18:40 -0700

    adding divisionquery data

---
+++ sqlscripts/manualsqlqueriesforforeignkeys/divisionquerydata.sql
@@ -0,0 +1,9 @@
+INSERT INTO `admin` (`id`, `name`, `email`, `password`, `country`) VALUES
+(1, 'test', 'test@test.com', 'password', 'Canada');
+
+
+INSERT INTO `browses` (`id`, `location`, `rname`) VALUES
+(5, 'Vancouver', 'Anton\'s Pasta'),
+(5, 'Vancouver', 'italiano'),
+(5, 'Vancouver', 'Italiano2'),
+(5, 'Vancouver', 'Pizza Locale');
